fbd8ddebc6cd85013192438c7407f0dd1f30741c
Adam Babcock <babada@mrhen.com>
2017-10-21 17:40:21 -0500

    Express hello world

---
+++ src/express-server/index.ts
@@ -0,0 +1,11 @@
+import * as express from 'express'
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('Hello, world!\n')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
