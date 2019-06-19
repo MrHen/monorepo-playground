607f2c05b1f73483e4166739692a5287f0055091
Adam Babcock <github@mrhen.com>
2019-06-18 21:52:08 -0500

    antd minimal example

--- src/react-antd-js-client/src/App.js
+++ src/react-antd-js-client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import TableDemo from './TableDemo';
+
 import './App.css';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
@@ -10,14 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TableDemo />
       </header>
     </div>
   );
